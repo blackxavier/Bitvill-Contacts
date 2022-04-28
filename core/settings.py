0c1ab4fd4aa223645a80983e2cfb10bb20c191d5
blackxavier <ogbuchiarinze01@gmail>
2022-04-28 18:02:57 +0100

    Added swagger documentation

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "contacts.apps.ContactsConfig",
     "rest_framework",
     "rest_framework.authtoken",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Contact List API",
+        default_version="v1",
+        description="An API for storing contacts",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@contacts.remote"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
@@ -8,7 +24,13 @@ urlpatterns = [
         include("accounts.urls", namespace="accounts"),
     ),
     path(
-        "",
+        "contacts/",
         include("contacts.urls", namespace="contacts"),
     ),
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
