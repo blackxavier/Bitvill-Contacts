604eb3ced533fbe4321fd7839e69fa8563234472
blackxavier <ogbuchiarinze01@gmail>
2022-05-17 14:49:21 +0100

    NC

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ environ.Env.read_env(Path(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("debug")
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
